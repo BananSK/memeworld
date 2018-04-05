64a871648bf2b4a188f941b6a0f0ec1b88fe1e4a
BqnanSK <bluegamer028@gmail.com>
2018-04-05 09:11:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -62,4 +62,4 @@ client.on('guildMemberRemove', member => {
     message.channel.sendMessage('bar');
   }
 });
- client.login("NDE5NjI2ODE1MTU4NjE2MDY0.DXy3cg.9lksdxDTIqGWTakOaCZdZbuT6U0");
+ client.login(process.env.token);
